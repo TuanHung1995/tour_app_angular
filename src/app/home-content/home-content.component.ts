460179b70b77a3c6629075df87afbd5388f1dcb1
TuanHung1995 <tuanhungg1995@gmail.com>
2024-10-12 22:12:22 +0700

    completed ui for home page

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -20,6 +20,13 @@ interface Tour {
   price: number;
 }
 
+interface News {
+  id: number;
+  title: string;
+  image: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-home-content',
   standalone: true,
@@ -29,6 +36,28 @@ interface Tour {
 })
 export class HomeContentComponent {
 
+
+  mockNews: News[] = [
+    {
+      id: 1,
+      title: 'Tin tức 1',
+      image: 'assets/images/photo1.webp',
+      content: 'Với lịch sử gần 700 năm, ngôi làng pháo đài Naganeupseong của Hàn Quốc khiến người ta ngỡ ngàng bởi thời gian dường như ngừng lại ở nơi đây..',
+    },
+    {
+      id: 2,
+      title: 'Tin tức 2',
+      image: 'assets/images/photo2.webp',
+      content: 'Với lịch sử gần 700 năm, ngôi làng pháo đài Naganeupseong của Hàn Quốc khiến người ta ngỡ ngàng bởi thời gian dường như ngừng lại ở nơi đây..',
+    },
+    {
+      id: 3,
+      title: 'Tin tức 3',
+      image: 'assets/images/photo3.webp',
+      content: 'Với lịch sử gần 700 năm, ngôi làng pháo đài Naganeupseong của Hàn Quốc khiến người ta ngỡ ngàng bởi thời gian dường như ngừng lại ở nơi đây..',
+    },
+  ];
+
   tours: Tour[] = [
     { id: 1, name: 'Tour Châu Âu 9N8Đ: Đức - Hà Lan - Bỉ - Pháp - Thụy Sỹ', image: 'assets/images/photo1.webp', startDate: '28/08/2023', duration: '9N8Đ', price: 136800000 },
     { id: 2, name: 'Tour Bờ Đông Hoa Kỳ 10N9Đ: New York - Philadelphia -...', image: 'path_to_usa_image.jpg', startDate: '28/08/2023', duration: '10N9Đ', price: 160000000 },
@@ -36,7 +65,19 @@ export class HomeContentComponent {
     { id: 4, name: 'Tour Cao Cấp Indonesia 4N3Đ: Thiên Đường Biển Đảo Bali', image: 'path_to_danang_image.jpg', startDate: '27/08/2023', duration: '4N3DD', price:2798000},
     { id: 5, name: 'Tour Đà Lạt 4N3Đ: Đà Lạt - Nha Trang - Thành Phố Hoa Biển', image: 'path_to_danang_image.jpg', startDate: '25/08/2023', duration: '4N3Đ', price:8600000},
     { id: 6, name: 'Tour Đà Lạt 3N2Đ : Đà Lạt - Thành Phố Ngàn Hoa', image: 'path_to_danang_image.jpg', startDate: '29/08/2023', duration: '3N2Đ', price:6600000},
+    { id: 6, name: 'Tour Đà Lạt 3N2Đ : Đà Lạt - Thành Phố Ngàn Hoa', image: 'path_to_danang_image.jpg', startDate: '29/08/2023', duration: '3N2Đ', price:6600000},
+    { id: 6, name: 'Tour Đà Lạt 3N2Đ : Đà Lạt - Thành Phố Ngàn Hoa', image: 'path_to_danang_image.jpg', startDate: '29/08/2023', duration: '3N2Đ', price:6600000},
+    { id: 6, name: 'Tour Đà Lạt 3N2Đ : Đà Lạt - Thành Phố Ngàn Hoa', image: 'path_to_danang_image.jpg', startDate: '29/08/2023', duration: '3N2Đ', price:6600000},
+    { id: 6, name: 'Tour Đà Lạt 3N2Đ : Đà Lạt - Thành Phố Ngàn Hoa', image: 'path_to_danang_image.jpg', startDate: '29/08/2023', duration: '3N2Đ', price:6600000},
+
   ];
+
+  vietnamTours: Tour[] = [
+      { id: 1, name: 'Tour Châu Âu 9N8Đ: Đức - Hà Lan - Bỉ - Pháp - Thụy Sỹ', image: 'assets/images/photo1.webp', startDate: '28/08/2023', duration: '9N8Đ', price: 136800000 },
+      { id: 2, name: 'Tour Bờ Đông Hoa Kỳ 10N9Đ: New York - Philadelphia -...', image: 'path_to_usa_image.jpg', startDate: '28/08/2023', duration: '10N9Đ', price: 160000000 },
+      { id: 3, name: 'Tour Hoa Kỳ 6N5Đ: Hawaii - Honolulu - Đảo Thiên Đường', image: 'path_to_hawaii_image.jpg', startDate: '27/08/2023', duration: '6N5Đ', price: 193000000 },
+      { id: 4, name: 'Tour Cao Cấp Indonesia 4N3Đ: Thiên Đường Biển Đảo Bali', image: 'path_to_danang_image.jpg', startDate: '27/08/2023', duration: '4N3DD', price:2798000},
+    ];
 
   bestTours: BestTour[] = [
     {
@@ -118,4 +159,20 @@ export class HomeContentComponent {
     '../../assets/images/hong_kong_sale.webp',
     'assets/images/tay_bac_sale.webp',
   ];
+
+  currentSlide = 0;
+
+  nextTour() {
+    const totalTours = 4; // Change this to the number of tours you have
+    if (this.currentSlide < totalTours - 1) {
+      this.currentSlide++;
+    }
+  }
+
+  prevTour() {
+    if (this.currentSlide > 0) {
+      this.currentSlide--;
+    }
+  }
+
 }
