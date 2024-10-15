774f1826a95bb92ca05c7de1384f46604caff60e
TuanHung1995 <tuanhungg1995@gmail.com>
2024-10-15 12:11:25 +0700

    logic for news

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { TourComponent } from './tour/tour.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
-  {path: 'tour', component: TourComponent}
+  {path: 'tour', component: TourComponent},
+  {path: 'login', component: LoginComponent}
 ];
---
+++ src/app/services/news.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { News } from '../models/news';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  
+  private apiUrl = environment.apiUrl + '/api/v1/news';
+
+  constructor(private http: HttpClient) { }
+
+  // Lấy tất cả các News
+  getAllNews(): Observable<News[]> {
+    return this.http.get<News[]>(this.apiUrl);
+  }
+
+  // Lấy News theo ID
+  getNewsById(id: number): Observable<News> {
+    return this.http.get<News>(`${this.apiUrl}/${id}`);
+  }
+
+  // Tạo mới News
+  createNews(news: News): Observable<News> {
+    return this.http.post<News>(this.apiUrl, news);
+  }
+
+  // Cập nhật News
+  updateNews(id: number, news: News): Observable<News> {
+    return this.http.put<News>(`${this.apiUrl}/${id}`, news);
+  }
+
+  // Xóa News
+  deleteNews(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  // Phân trang News theo trạng thái
+  getAllNewsByStatus(status: string, page: number, size: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/page`, {
+      params: {
+        status: status,
+        page: page.toString(),
+        size: size.toString()
+      }
+    });
+  }
+
+  // Lấy News theo danh mục và trạng thái
+  getAllNewsByCategoryAndStatus(category: string, status: string): Observable<News[]> {
+    return this.http.get<News[]>(`${this.apiUrl}/category`, {
+      params: {
+        category: category,
+        status: status
+      }
+    });
+  }
+
+}
